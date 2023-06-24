efdfce96db166176747356c9d5fa43127612b299
Ankush Agrawal <ankushagrawal4321@gmail.com>
2023-06-24 12:57:34 +0530

    we are adding a new project name blog website

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useContext, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../userContext';
+
+
+const LoginPage = () => {
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [redirect,setRedirect] = useState(false);
+  const {setUserInfo} = useContext(UserContext);
+
+ async function login(ev){
+    ev.preventDefault();
+  const response =  await fetch('http://localhost:4000/login',{
+      method:'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+      credentials:'include'
+    })
+    console.log(response);
+    if(response.ok){
+        response.json().then(userInfo=>{
+          setUserInfo(userInfo);
+          setRedirect(true);
+        })
+    }else{
+      if(response.status=== 404){
+        alert('Invalid User. Please try with valid user information');
+      }
+      else{ 
+        alert('Invalid password. plz check the password')
+      }
+    }
+  }
+    if(redirect){
+      return <Navigate to ={'/'}/>
+    }
+   return (
+    <div>
+        <form className='login' onSubmit={login}>
+            <h1>Login Page</h1>
+            <input type='text' placeholder='Username' value={username} onChange={ev=>{setUsername(ev.target.value)}}></input>
+            <input type='password' placeholder='Password'value={password} onChange={ev=>{setPassword(ev.target.value)}}></input>
+            <button>Login</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const RegisterPage = () => {
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+
+  async function register(ev){
+    ev.preventDefault();
+   
+     const response =  await fetch('http://localhost:4000/register',{
+      method:'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type' : 'application/json'}
+    })
+    
+    if(response.status === 200){
+      alert('Registration Successful');
+    }else{
+      alert('Registration Failed')
+    }
+    
+  }
+
+  return (
+    <div>
+        <form className='register' onSubmit={register}>
+            <h1>Register Page</h1>
+        <input type='text' placeholder='username'
+         value={username}
+         onChange={ev=>setUsername(ev.target.value)}></input>
+        <input type='password'
+         placeholder='password'
+         value={password}
+         onChange={ev=>setPassword(ev.target.value)}
+        ></input>
+        <button>Register</button>
+        </form>
+    </div>
+  )
+}
+
+export default RegisterPage
